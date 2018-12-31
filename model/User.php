f19d3d9819151578bbec4d1407c032f93f2e24b3
FretardLoic <fretardloic@gmail.com>
2018-12-31 15:36:12 +0100

    Petit pas ...

--- model/User.php
+++ model/User.php
@@ -1,13 +1,26 @@
 <?php
 class User {
+  private static $addUserQuery = 'INSERT INTO USERS (LOGIN, PSEUDO, PASSWORD, ADMIN)'.
+      'VALUES (:login, :pseudo, :pass, :admin)';
+  private static $getUserQuery = 'SELECT LOGIN, PSEUDO, PASSWORD, ADMIN'.
+      'FROM USERS WHERE LOGIN = :login';
+  private static $setPseudoUserQuery = 'UPDATE USERS SET PSEUDO = :pseudo'.
+      'WHERE ';
+
   private $login;
   private $pseudo;
   private $passwd;
+  private $admin;
 
-  public function __construct($login, $pseudo, $passwd) {
+  public function __construct($login, $passwd, $pseudo = '', $admin = false) {
     $this->login = $login;
     $this->pseudo = $pseudo;
     $this->passwd = $passwd;
+    $this->admin = $admin;
+  }
+
+  public static function getUser($login) {
+
   }
 
 }
